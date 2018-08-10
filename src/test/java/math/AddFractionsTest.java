e2e005427fb2047c84b784abbd1ea67718ebc742
JeanLuc001 <sebastian.kramer@web.de>
2018-08-10 11:04:01 +0200

    we now add negative integers, even when it gives negative result

--- src/test/java/math/AddFractionsTest.java
+++ src/test/java/math/AddFractionsTest.java
@@ -7,17 +7,15 @@ import org.junit.Test;
 public class AddFractionsTest
 {
 	/**
-	 * Dieser Test hat kein "Warum", aber ein "Was" (0+0=0)
-	 * Frage: Ist das WIRKLICH einfacher, intValue() statt
-	 * numerator() abzufragen? Ich WEISS doch, dass es um Brueche geht.
-	 * Ist es ausserdem angemessen, Fraction mit nur EINEM Wert statt
-	 * zwei Werten zu instanziieren, wie ich es als User von Breuchen
-	 * erwarten wuerde (es gibt numerator und denominator)?
-	 * Diese Art von Test widerspricht sich selbst: Die Klasse
-	 * Fraction gibt gemaess ihres Namens vor, ein Bruch zu sein,
-	 * sieht aber mit ihrer intValue()-Methode und ihrem Konstuktor
-	 * aus wie ein Integer oder eine Number.
-	 * Das macht das Verstaendnis doch nicht wirklich einfacher, oder?
+	 * Dieser Test hat kein "Warum", aber ein "Was" (0+0=0) Frage: Ist das
+	 * WIRKLICH einfacher, intValue() statt numerator() abzufragen? Ich WEISS
+	 * doch, dass es um Brueche geht. Ist es ausserdem angemessen, Fraction mit
+	 * nur EINEM Wert statt zwei Werten zu instanziieren, wie ich es als User
+	 * von Breuchen erwarten wuerde (es gibt numerator und denominator)? Diese
+	 * Art von Test widerspricht sich selbst: Die Klasse Fraction gibt gemaess
+	 * ihres Namens vor, ein Bruch zu sein, sieht aber mit ihrer
+	 * intValue()-Methode und ihrem Konstuktor aus wie ein Integer oder eine
+	 * Number. Das macht das Verstaendnis doch nicht wirklich einfacher, oder?
 	 */
 	@Test
 	public void zerosPlusZeros() throws Exception
@@ -27,10 +25,10 @@ public class AddFractionsTest
 	}
 
 	/**
-	 * nonZeroPlusZero statt threePlusZero, weil die Bedeutung "three"
-	 * nicht klar wuerde.
-	 * Frage: ginge auch whenAddingZeroToNonZeroThenExpectCorrectResult()?
-	 * Zu Wort gewaltig? Aber given-when-then konform! 
+	 * nonZeroPlusZero statt threePlusZero, weil die Bedeutung "three" nicht
+	 * klar wuerde. Frage: ginge auch
+	 * whenAddingZeroToNonZeroThenExpectCorrectResult()? Zu Wort gewaltig? Aber
+	 * given-when-then konform!
 	 */
 	@Test
 	public void nonZeroPlusZero() throws Exception
@@ -38,18 +36,25 @@ public class AddFractionsTest
 		Fraction sum = new Fraction(3).plus(new Fraction(0));
 		assertEquals(3, sum.intValue());
 	}
-	
+
 	@Test
 	public void zeroPlusNonZero() throws Exception
 	{
 		Fraction sum = new Fraction(0).plus(new Fraction(5));
 		assertEquals(5, sum.intValue());
 	}
-	
+
 	@Test
 	public void nonNegativeNonZeroOperands() throws Exception
 	{
 		Fraction sum = new Fraction(3).plus(new Fraction(4));
 		assertEquals(7, sum.intValue());
+	}
+
+	@Test
+	public void negativeInputsAndNegativeOutput() throws Exception
+	{
+		Fraction sum = new Fraction(-3).plus(new Fraction(1));
+		assertEquals(-2, sum.intValue());
 	}
 }
