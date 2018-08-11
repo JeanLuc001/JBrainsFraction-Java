5d3bee2a0b1d13bb16a67cdb9d4244fcc58e68f0
JeanLuc001 <sebastian.kramer@web.de>
2018-08-11 16:43:15 +0200

    Explored GCD algorithm from Wikipedia; found annoying aspect of its contract.

--- src/test/java/math/GreatestCommonDivisorTest.java
+++ src/test/java/math/GreatestCommonDivisorTest.java
@@ -4,12 +4,54 @@ import static org.junit.Assert.*;
 
 import org.junit.Test;
 
+/**
+ * Der Sinn dieser Klasse ist nicht TDD, sondern sicherzustellen, dass wir die
+ * gcd()-Methode, kopiert von Wikipedia, richtig implementieren.<br>
+ * Daher sind die Methoden keine TDD-Tests, sondern eher Beispiele und
+ * beinhalten u.U. mehrere Asserts.
+ *
+ */
 public class GreatestCommonDivisorTest
 {
 	@Test
-	public void oneAndOne() throws Exception
+	public void reflexive() throws Exception
 	{
 		assertEquals(1, gcd(1, 1));
+		assertEquals(2, gcd(2, 2));
+		assertEquals(-1, gcd(-1, -1));
+	}
+
+	@Test
+	public void testName() throws Exception
+	{
+		assertEquals(1, gcd(2, 3));
+		assertEquals(1, gcd(4, 7));
+		assertEquals(-1, gcd(-2, -3));
+	}
+
+	@Test
+	public void oneIsMultipleOfTheOther() throws Exception
+	{
+		assertEquals(3, gcd(3, 9));
+		assertEquals(5, gcd(5, 30));
+	}
+	
+	@Test
+	public void commonFactor() throws Exception
+	{
+		assertEquals(2, gcd(6, 8));
+		assertEquals(7, gcd(49, 315));
+		assertEquals(-4, gcd(-24, -28));
+		assertEquals(4, gcd(-24, 28));
+	}
+	
+	@Test
+	public void negatives() throws Exception
+	{
+		// Do I like this behaviour of the standard algo?
+		// Probably prefer gcd > 0
+		assertEquals(-4, gcd(-24, -28));
+		assertEquals(4, gcd(-24, 28));
 	}
 
 	private int gcd(int a, int b)
